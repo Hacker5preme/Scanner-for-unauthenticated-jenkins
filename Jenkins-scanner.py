84227e53b7d0f8217d74b40e76a9c7eea7c9d7b0
Hacker5preme <54862244+Hacker5preme@users.noreply.github.com>
2021-03-31 15:08:46 +0200

    Update Jenkins-scanner.py

--- Jenkins-scanner.py
+++ Jenkins-scanner.py
@@ -5,7 +5,7 @@ ip = sys.argv[1]
 port = sys.argv[2]
 protocol = sys.argv[3]
 
-response = requests.get(protocol + '://' + ip + ':' + port).status_code
+response = requests.get(protocol + '://' + ip + ':' + port + '/script').status_code
 if response == 200:
     print(ip + ' is vulnerable on Port: ' + port)
 else:
